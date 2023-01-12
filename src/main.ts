c6b8550d82e9a4f420cabe24a9de9e68a942c6da
zhangsan <zhangsan@.com>
2023-01-13 00:11:47 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import i18n from './message'
-import './responsive'
 import 'virtual:svg-icons-register'
 import svgicon from './components/svg/SvgIcon.vue'
 
